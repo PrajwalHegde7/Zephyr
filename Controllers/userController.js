8c451ee3ebe02568fb3bb3cb831de95222934e23
Prajwal Hegde <prajwalsh234@gmail.com>
2023-09-01 17:52:10 +0530

    Initial commit

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -7,7 +7,10 @@ const login = (req, res) => {
         res.send("Enter all the details");
     } else {
         if (email === "admin@zephyr.com" && pass === "admin") {
-            const token = jwt.sign({ email }, "secret123secret");
+            const token = jwt.sign({ email }, "secret123secret", {
+                expiresIn: "5m",
+            });
+            console.log(token);
             res.appendHeader("token", token);
             res.sendFile(
                 __dirname.split(path.sep).slice(0, -1).join(path.sep) +
